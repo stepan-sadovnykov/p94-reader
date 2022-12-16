3fac1e288aceb9b573f8a3dee8af4cfbd6572ee3
Stepan Sadovnykov <sadovnikovss@gmail.com>
2022-12-17 01:17:00 +0200

    Working draft

---
+++ main.js
@@ -0,0 +1,18 @@
+export async function upload(event) {
+    /** @type HTMLInputElement */
+    const target = event.target;
+    /** @type File */
+    const file = target.files[0];
+    const element = document.createElement("template");
+    document.createDocumentFragment().append(element);
+    element.innerHTML = await file.text();
+    /** @type HTMLMetaElement */
+    const contentMeta = element.content.getElementById("meta-preload-data");
+    const contentJson = JSON.parse(contentMeta.getAttribute("content"));
+    let content = Object.entries(contentJson.novel).values().next().value[1].content;
+    content = content
+        .replaceAll("\r", "")
+        .replaceAll("\n", "<br/>")
+        .replaceAll("[newpage]", "<hr/>");
+    document.getElementById("text").innerHTML = content;
+}
